4440bfc40bc691bc7be68af13cf2fb96e7d90f7c
aavidsoft-developer <aavidsoft.developer@gmail.com>
2023-07-25 16:10:22 +0530

    Singleton Design pattern

---
+++ src/Maths.java
@@ -0,0 +1,22 @@
+public class Maths {
+    private static Maths maths = null;
+
+    private Maths() {
+    }
+
+    public static Maths getInstance() {
+        if(maths == null) {
+            maths = new Maths();
+        }
+        return maths;
+    }
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int sub(int num1, int num2) {
+        return num1 - num2;
+    }
+
+}
